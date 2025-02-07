6dd9a8b866f5aeaf6280689b45bceb2f2355c796
Андріана-Ольга Пиць <andriana-olha.pyts@lnu.edu.ua>
2025-02-07 16:57:43 +0200

    Add page statistic

---
+++ Controllers/SalesStatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using Cinema.Models.DataBaseModels;
+using Cinema.Repository.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Controllers
+{
+    [Authorize(Roles = "Admin")] // Доступ тільки для адміністратора
+    public class SalesStatisticsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SalesStatisticsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Statistic()
+        {
+            var salesStatistics = await _unitOfWork.SalesStatistics.GetAllAsync();
+
+            // Отримуємо всі сеанси та статистику для кожного сеансу
+            var sessionStatistics = salesStatistics.Select(stat => new
+            {
+                stat.Session.Movie.Title,
+                stat.Session.StartTime,
+                stat.TicketsSold,
+                stat.Revenue
+            }).ToList();
+
+            // Передаємо дані в представлення
+            return View(sessionStatistics);
+        }
+    }
+}
